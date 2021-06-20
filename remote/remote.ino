9306157064955447a46f2899896b36b4cb0ca9f7
Michal Kovacik <gashtaan@gmail.com>
2021-06-20 18:53:38 +0200

    Fixed start program parameter, programs are numbered from zero

--- remote/remote.ino
+++ remote/remote.ino
@@ -98,14 +98,14 @@ void web_mainPage()
 			Start program:<br>
 			<form method="get" action="/startProgram">
 				<select name="program">
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-					<option value="6">6</option>
-					<option value="7">7</option>
-					<option value="8">8</option>
+					<option value="0">1</option>
+					<option value="1">2</option>
+					<option value="2">3</option>
+					<option value="3">4</option>
+					<option value="4">5</option>
+					<option value="5">6</option>
+					<option value="6">7</option>
+					<option value="7">8</option>
 				</select>
 				<input type="submit" value="Start">
 			</form>
